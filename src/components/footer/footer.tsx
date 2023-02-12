29fc7c870d657502b67816c6f9efbcc45d41820e
Kennedy <aarontkennedy@gmail.com>
2023-02-12 15:23:44 -0600

    finish footer text

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Navigation from "../navigation/navigation";
 import SocialNavigation from "../socialNavigation/socialNavigation";
 import "./footer.scss";
@@ -8,19 +7,21 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer__column">
-        <div>Rail Werks Brewing Depot &trade;</div>
-        <div>
+        <div className="footer__title footer__brand-name">
+          Rail Werks Brewing Depot &trade;
+        </div>
+        <div className="footer__value">
           <a href="mailto:info@railwerksbrewingdepot.com">
             info@railwerksbrewingdepot.com
           </a>
         </div>
         <div>
-          <h4>Hours</h4>
-          <p>TBD</p>
+          <h4 className="footer__title">Hours</h4>
+          <div className="footer__value">TBD</div>
         </div>
         <div>
-          <h4>Location</h4>
-          <p>Columbia Heights, MN</p>
+          <h4 className="footer__title">Location</h4>
+          <div className="footer__value">Columbia Heights, MN</div>
         </div>
       </div>
 
